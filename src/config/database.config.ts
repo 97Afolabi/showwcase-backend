cd09f9485669cef42c36222f43744239096b4491
Afolabi Samuel Adedeji <97afolabi@gmail.com>
2023-07-26 08:35:35 +0100

    feat: set up Postgres DB

---
+++ src/config/database.config.ts
@@ -0,0 +1,11 @@
+import { Client } from "pg";
+
+const dbConfig = new Client({
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+});
+
+export default dbConfig;
